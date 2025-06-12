5a2970797f8e0e5dcaf491fd4400c38870c5112a
Jacob Gilbert <46575360+JacobWG@users.noreply.github.com>
2025-06-11 20:09:15 -0700

    Updated in-progress pages.

--- app/routes/projects.tsx
+++ app/routes/projects.tsx
@@ -1,13 +1,6 @@
 import * as React from "react";
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Button from '@mui/material/Button';
-import {Link} from 'react-router';
-import {SamplesRoutes} from "~/components/SamplesData";
-import Divider from '@mui/material/Divider';
 
 export default function Projects() {
     return (
@@ -18,32 +11,7 @@ export default function Projects() {
             <Typography sx={{ marginBottom: 2 }}>
                 The projects listed here were completed entirely by me or led by me. Work is shown where I'm able to, however several of these projects were internal to Zonar.
             </Typography>
-            <Box sx={{display: 'flex'}}>
-                {SamplesRoutes.map((item, index) => (
-                    <Card key={index}
-                          elevation={2}
-                          sx={{
-                              margin: '1rem',
-                              minWidth: '10rem',
-                              maxWidth: '20%',
-                              display: 'flex',
-                              flexFlow: 'column',
-                              justifyContent: 'space-between'
-                          }}>
-                        <CardContent>
-                            <Box sx={{display: 'flex', justifyContent: 'space-between', paddingBottom: '0.5rem'}}>
-                                <Typography variant="h5">{item.title}</Typography>
-                                <Typography variant="h6">{item.topic}</Typography>
-                            </Box>
-                            <Divider />
-                            <Typography sx={{paddingTop: "1rem"}}>{item.text}</Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button component={Link} to={item.path}>View Article</Button>
-                        </CardActions>
-                    </Card>
-                ))}
-            </Box>
+            <Typography variant={'h3'}>In progress.</Typography>
         </Box>
     )
 }
--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 
 export default function Resume() {
     return (
-        <Box id={"title"} sx={{display: 'block', minHeight: '50vh'}}>
-            <Typography variant="h1" sx={{ marginBottom: 2 }}>
-                Resume
-            </Typography>
-            <Typography variant='h2' sx={{ marginBottom: 2 }}>
-                Portfolio
-            </Typography>
+        <Box id="title" sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            minHeight: '35vh',
+            width: 'calc(100vw - 200px)'
+        }}>
+            <Typography variant={'h2'}>Resume</Typography>
+            <Typography>Currently in progress. Until then, please see <Link href={'https://docs.google.com/document/d/1IFNZhp3LLeMqzpSWzHQPz3I3JLmUYja6QJceGMEqmww/edit?usp=sharing'} target={'blank'}>the Google Drive version</Link>.</Typography>
         </Box>
     )
 }
